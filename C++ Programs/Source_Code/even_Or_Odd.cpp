ad9b026eb9aa9086df87ec22df7da51fae3b7a59
ANKIT <ankitrjrkn@gmail.com>
2022-01-31 17:47:50 +0530

    Added Even_Or_Odd program

---
+++ C++ Programs/Source_Code/even_Or_Odd.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long int input1, input2;
+	char choose;
+	cout << "***************************************************************************" << endl;
+	cout << "PROGRAM TO PRINT ALL EVEN OR ODD NUMBERS BETWEEN GIVEN RANGE OF NUMBERS :- " << endl;
+	cout << "***************************************************************************" << endl << endl;
+	cout << "Enter the Smallest Number among the two :- " << endl;
+	cin >> input1;
+	cout << "Your input is = " << input1 << endl << endl;
+	cout << "Enter the Largest Number among the two :- " << endl;
+	cin >> input2;
+	cout << "Your input is = " << input2 << endl << endl;
+	cout << "Which series you wanna print ? Even or Odd {Press E (in capital) for Even or O (in capital) for Odd}" << endl;
+	cin >> choose;
+	cout << "Your input is = " << choose << endl << endl;
+	if (choose == 'E' )
+	{
+		if (input1 % 2 == 0)
+		{
+			cout << "Series is as follows :-" << endl;
+			while (input1 < input2 - 2)
+			{
+				cout << input1 + 2 << endl;
+				input1 = input1 + 2;
+			}
+		}
+		else
+		{
+			cout << "Series is as follows :-" << endl;
+			while (input1 < input2 - 1 )
+			{
+				cout << input1 + 1 << endl;
+				input1 = input1 + 2;
+			}	
+		}
+	}
+	else if (choose == 'O' )
+	{
+		if (input1 % 2 == 0)
+		{
+			cout << "Series is as follows :-" << endl;
+			while (input1 < input2 -1 )
+			{
+				cout << input1 + 1 << endl;
+				input1 = input1 + 2;
+			}
+		}
+		else
+		{
+			cout << "Series is as follows :-" << endl;
+			while (input1 < input2 - 2 )
+			{
+				cout << input1 + 2 << endl;
+				input1 = input1 + 2;
+			}	
+		}
+	}
+	else
+	{
+	cout << "INVALID INPUT";
+	}
+return 0;
+}
